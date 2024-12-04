1740259ccb53e8d1c6e4b1ad69b18d516edeee11
Marcos-Linux-Windows <willianmw@hotmail.com>
2024-12-03 21:47:54 -0300

    changes for new funcitons and some features for mouse clicks and step. [2] fix, the first commit dont up

---
+++ src/hooks/key_hook.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_hook.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malopes- <malopes-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 20:14:01 by malopes-          #+#    #+#             */
+/*   Updated: 2024/12/01 20:14:01 by malopes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../fdf.h"
+
+int	key_hook(int keycode, t_fdf_gen *gen_data)
+{
+    if (keycode == 65307 || keycode == 53)
+        return (mlx_loop_end(gen_data->data->mlx_ptr));
+        //return(free_resources(gen_data));
+    if (keycode == 105) // Tecla 'i' para aumentar a profundidade
+        gen_data->graph->depth_factor += 0.4;
+    if (keycode == 111) // Tecla 'o' para diminuir a profundidade
+        gen_data->graph->depth_factor -= 0.4;
+    if (keycode == 65362) // Seta para cima rotaciona para cima
+        gen_data->graph->angle_x += 0.1;
+    if (keycode == 65364) // Seta para baixo rotaciona para baixo
+        gen_data->graph->angle_x -= 0.1;
+    if (keycode == 65361) // Seta para esquerda rotaciona girando para esquerda
+        gen_data->graph->angle_y -= 0.1;
+    if (keycode == 65363) // Seta para direita rotaciona girando para direita
+        gen_data->graph->angle_y += 0.1;
+    if (keycode == 119) // tecla w movimenta para cima
+        gen_data->graph->shift_y -= 10;
+    if (keycode == 115) // tecla s movimenta para baixo
+        gen_data->graph->shift_y += 10;
+    if (keycode == 97) // tecla a movimenta para esquerda
+        gen_data->graph->shift_x -= 10;
+    if (keycode == 100) // tecla d movimenta para direita
+        gen_data->graph->shift_x += 10;
+    if (keycode == 122) // tecla z da zoom
+        gen_data->graph->zoom += 5;
+    if (keycode == 120) // tecla x diminui o zoom
+        gen_data->graph->zoom -= 5;
+    draw(gen_data->data, gen_data->graph, gen_data->img_data);
+    printf("%d\n", keycode);
+    return (0);
+}
---
+++ src/hooks/mouse_control.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mouse_control.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malopes- <malopes-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 20:14:14 by malopes-          #+#    #+#             */
+/*   Updated: 2024/12/01 20:14:14 by malopes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../fdf.h"
+
+int set_mouse_up(int button, int x, int y, void *param) {
+	t_fdf_gen *gen_data;
+
+	(void)x;
+	(void)y;
+	gen_data = (t_fdf_gen *)param;
+	if (!gen_data || !gen_data->mouse_set) {
+		write(2, "Erro: param é NULL em set_mouse_up\n", 35);
+		return (0);
+	}
+
+	if (button == 1) {
+		write(1, "Mouse up detected\n", 18);
+		gen_data->mouse_set->click_active = 0;
+	}
+	return (0);
+}
+
+int set_mouse_down(int button, int x, int y, void *param) {
+	t_fdf_gen *gen_data;
+
+	gen_data = (t_fdf_gen *)param;
+	if (!gen_data || !gen_data->mouse_set) {
+		write(2, "Erro: param é NULL em set_mouse_down\n", 36);
+		return (0);
+	}
+
+	if (button == 1) {
+		write(1, "Mouse press\n", 13);
+		gen_data->mouse_set->click_active = 1;
+		gen_data->mouse_set->last_mouse_x = x;
+		gen_data->mouse_set->last_mouse_y = y;
+	}
+	return (0);
+}
+
+int mouse_move_event(int x, int y, void *param) 
+{
+	t_fdf_gen *gen_data;
+	int dx;
+	int dy;
+
+	gen_data = (t_fdf_gen *)param;
+	if (!gen_data || !gen_data->mouse_set || !gen_data->graph) {
+		write(2, "Erro: param ou graph é NULL em mouse_move_event\n", 49);
+		return (0);
+	}
+	if (!gen_data->mouse_set->click_active)
+		return (0);
+	dx = x - gen_data->mouse_set->last_mouse_x;
+	dy = y - gen_data->mouse_set->last_mouse_y;
+
+	if (dx != 0 || dy != 0) {
+		gen_data->graph->angle_x += dy * 0.005;
+		gen_data->graph->angle_y += dx * 0.005;
+		gen_data->mouse_set->last_mouse_x = x;
+		gen_data->mouse_set->last_mouse_y = y;
+		draw(gen_data->data, gen_data->graph, gen_data->img_data);
+	}
+	return (0);
+}
